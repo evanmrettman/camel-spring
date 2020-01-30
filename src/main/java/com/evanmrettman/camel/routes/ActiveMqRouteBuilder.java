3c00133fb91bd202fd61e0fc9c2f07193284c0f7
Evan Rettman <erettman@masonlive.gmu.edu>
2020-01-30 14:14:01 -0500

    routebuilder configure

---
+++ src/main/java/com/evanmrettman/camel/routes/ActiveMqRouteBuilder.java
@@ -0,0 +1,13 @@
+package com.evanmrettman.camel.routes;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class ActiveMqRouteBuilder extends RouteBuilder
+{
+    public void configure()
+    {
+        from("activemq:queue:start")
+                .to("bean:testBean?method=hello")
+                .to("stream:out");
+    }
+}
