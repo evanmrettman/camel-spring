7c04d16e22da8d09658e04780910a986dcc3c276
Evan Rettman <erettman@masonlive.gmu.edu>
2020-01-30 17:30:07 -0500

    multiple routes via spring

---
+++ src/main/java/com/evanmrettman/camel/CamelMultipleRoutingExampleUsingSpring.java
@@ -0,0 +1,29 @@
+package com.evanmrettman.camel;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.spring.SpringCamelContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CamelMultipleRoutingExampleUsingSpring
+{
+    public static void main(String[] args) throws Exception
+    {
+        ConfigurableApplicationContext ctx_app = new ClassPathXmlApplicationContext("multipleRoutingApplicationContext.xml");
+        CamelContext ctx_camel = SpringCamelContext.springCamelContext(ctx_app,false);
+        try
+        {
+            ctx_camel.start();
+            Thread.sleep(3000);
+        }
+        catch (Exception e)
+        {
+            System.out.println("error occurred: "+e.toString());
+        }
+        finally
+        {
+            ctx_camel.stop();
+            ctx_app.close();
+        }
+    }
+}
